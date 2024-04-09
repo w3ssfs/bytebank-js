34bf38871ab77692d1339c6241a0fc9d05e513e2
Wesley <85897421+w3ssfs@users.noreply.github.com>
2024-04-08 21:32:38 -0300

    new date

--- script/workers/workerEuro.js
+++ script/workers/workerEuro.js
@@ -1,12 +1,10 @@
 async function conectaAPI() {
-  const conecta = await fetch(
-    "https://economia.awesomeapi.com.br/last/USD-BRL"
-  );
+  const conecta = await fetch("https://economia.awesomeapi.com.br/last/EURBRL");
   const conectaTraduzido = await conecta.json();
   postMessage(conectaTraduzido.USDBRL);
 }
 
 addEventListener("message", () => {
   conectaAPI();
-  setInterval(() => conectaAPI(), 10000);
+  setInterval(() => conectaAPI(), 5000);
 });
